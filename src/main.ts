7128e0b9a7bfe7ecb0e1c2c0d3630b255a2b382c
Piotr Proc <piotr.proc@gmail.com>
2025-07-21 12:33:00 +0200

    Change ESLint - change intend to 4

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,24 @@ import { addSpinButton } from "./components/spinButton.ts";
 const app = new Application();
 
 async function setup() {
-  // Intialize the application.
-  await app.init({ background: "#1099bb", resizeTo: window });
-
-  // Then adding the application's canvas to the DOM body.
-  document.body.appendChild(app.canvas);
+    await app.init({background: "#1099bb", resizeTo: window});
+    document.body.appendChild(app.canvas);
 }
 
 async function preload() {
-  // Create an array of asset data to load.
-  const assets = [
-    {
-      alias: "spinButton",
-      src: "./public/assets/spinButton.png",
-    },
-  ];
+    const assets = [
+        {
+            alias: "spinButton",
+            src: "./public/assets/spinButton.png",
+        },
+    ];
 
-  await Assets.load(assets);
+    await Assets.load(assets);
 }
 
 (async () => {
-  await setup();
-  await preload();
+    await setup();
+    await preload();
 
-  addSpinButton(app);
+    addSpinButton(app);
 })();
