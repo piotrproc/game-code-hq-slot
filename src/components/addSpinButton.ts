88fe0bca5ce57c9475d8576ba64fd9b04ae62924
Piotr Proc <piotr.proc@gmail.com>
2025-07-21 17:11:30 +0200

    Refactor 2

--- src/components/addSpinButton.ts
+++ src/components/addSpinButton.ts
@@ -1,15 +1,12 @@
 import { Application, Sprite } from "pixi.js";
-
-let isSpin = false;
+import { addSpinButtonClickHandler } from "./spin.ts";
 
 export function addSpinButton(app: Application, symbolSprites: Sprite[], reels: Sprite[]) {
     const spinButton = Sprite.from("spinButton");
 
     styleSpinButton(app, spinButton);
 
-    const changingSymbolsCallback = () => changeSymbol(symbolSprites, reels);
-
-    spinButton.on('pointerdown', () => handleClick(spinButton, app, changingSymbolsCallback));
+    spinButton.on('pointerdown', addSpinButtonClickHandler(app, spinButton, symbolSprites, reels));
 
     app.stage.addChild(spinButton);
 }
@@ -22,39 +19,4 @@ function styleSpinButton(app: Application, spinButton: Sprite) {
 
     spinButton.eventMode = 'static';
     spinButton.cursor = 'pointer';
-}
-
-function handleClick(spinButton: Sprite, app: Application, changingSymbolsCallback) {
-    if (isSpin) {
-        stopSpin(app, changingSymbolsCallback);
-        spinButton.tint = 0xFFFFFF;
-    } else {
-        spin(app, changingSymbolsCallback);
-        spinButton.tint = 0x808080;
-    }
-    isSpin = !isSpin;
-}
-
-function spin(app: Application, changingSymbolsCallback) {
-    app.ticker.add(changingSymbolsCallback)
-}
-
-function changeSymbol(symbolSprites: Sprite[], reels: Sprite[]) {
-    const randomIndexGenerator = () => Math.floor(Math.random() * reels.length)
-
-    const randomIndex = randomIndexGenerator();
-    const randomIndex2 = randomIndexGenerator();
-    const randomIndex3 = randomIndexGenerator();
-
-    changeSymbolSprite(symbolSprites[0], reels[randomIndex]);
-    changeSymbolSprite(symbolSprites[1], reels[randomIndex2]);
-    changeSymbolSprite(symbolSprites[2], reels[randomIndex3]);
-}
-
-function changeSymbolSprite(symbolSprite: Sprite, newSymbol: Sprite) {
-    symbolSprite.texture = newSymbol.texture;
-}
-
-function stopSpin(app: Application, changingSymbols) {
-    app.ticker.remove(changingSymbols);
 }
