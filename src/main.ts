346ade17a2b28789ca9192ef0d39f7bf17fbb305
Piotr Proc <piotr.proc@gmail.com>
2025-07-22 20:10:53 +0200

    Refactor name files

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,50 @@
-import { Application, Assets } from "pixi.js";
-
-const app = new Application();
-
-async function setup() {
-    await app.init({background: "#1099bb", resizeTo: window});
-    document.body.appendChild(app.canvas);
-}
-
-async function preload() {
-    const assets = [
-        {alias: "spinButton", src: "./public/assets/spinButton.png",},
-        {alias: "SYM3", src: "./public/assets/symbols/SYM3.png",},
-        {alias: "SYM4", src: "./public/assets/symbols/SYM4.png",},
-        {alias: "SYM5", src: "./public/assets/symbols/SYM5.png",},
-        {alias: "SYM6", src: "./public/assets/symbols/SYM6.png",},
-        {alias: "SYM7", src: "./public/assets/symbols/SYM7.png",},
-    ];
-
-    await Assets.load(assets);
-}
+import { Application, Assets, Texture, } from 'pixi.js';
+import { addSpinButton } from "./components/spinButton.ts";
+import { startPlay } from "./components/spinning.ts";
+import { addReels, addReelsSpinningHandler } from "./components/reels.ts";
+import { addTween } from "./components/tween.ts";
+import { addLimitsToGame } from "./components/limits.ts";
+import { getThemeConfig } from "./components/utils.ts";
 
 (async () => {
-    await setup();
-    await preload();
+    const config = getThemeConfig();
+    // Create a new application
+    const app = new Application();
 
+    // Initialize the application
+    await app.init({
+        background: config.background.color,
+        height: 1050,
+        width: 1200,
+    });
 
-    // buildReels(app);
-    // const reels = addReels(app);
-    // addSpinButton(app, reels.symbols, reels.allSymbols);
+    // Append the application canvas to the document body
+    document.body.appendChild(app.canvas);
+
+    // Load the textures
+    await Assets.load([
+        {alias: "spinButton", src: config.background.spinButton,},
+        ...config.symbols,
+    ]);
+
+    // // Create different slot symbols
+    const slotTextures = config.symbols.map(
+        symbol => Texture.from(symbol.src)
+    )
+
+    const {reels} = addReels(app, slotTextures);
+    const tweenTo = addTween(app);
+    const spinButton = addSpinButton(app);
+    spinButton.addListener('pointerdown', () => {
+        startPlay(reels, tweenTo, spinButton);
+    });
+
+    addLimitsToGame(app);
+    addReelsSpinningHandler(app, reels, slotTextures);
+
+    document.addEventListener('keypress', (e: KeyboardEvent) => {
+        if (e.key === ' ') {
+            startPlay(reels, tweenTo, spinButton);
+        }
+    });
 })();
