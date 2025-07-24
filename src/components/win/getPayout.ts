75230e9a4ae36e497988d22083361014499a0ce6
Piotr Proc <piotr.proc@gmail.com>
2025-07-24 12:36:36 +0200

    Refactor drawing betlines

--- src/components/win/getPayout.ts
+++ src/components/win/getPayout.ts
@@ -1,4 +1,6 @@
-export function getPayout(config, sprite) {
+import { Sprite } from "pixi.js";
+
+export function getPayout(config, sprite: Sprite) {
     const symbolName = sprite.texture.textureCacheIds[1];
 
     return config.symbols.filter(symbol => symbol.alias === symbolName)[0].payout
--- src/components/win/winChecker.ts
+++ src/components/win/winChecker.ts
@@ -7,7 +7,7 @@ export function checkWin(app: Application, config, reels, winElements) {
 
     const outcomeReels = reels.map((reel, indexY) => {
         return reel.symbols.filter((symbol) => {
-            return Math.round(symbol.x) > -75 && Math.round(symbol.x) < 375;
+            return Math.round(symbol.x) > -50 && Math.round(symbol.x) < 350;
         }).sort(comparisonFunction)
             .map((symbol, indexX) => {
                 symbol.reelX = indexX;
@@ -16,44 +16,46 @@ export function checkWin(app: Application, config, reels, winElements) {
             });
     })
 
-    checkBetlineWin(app, config, outcomeReels[0][0], outcomeReels[0][1], outcomeReels[0][2], winElements);
-    checkBetlineWin(app, config, outcomeReels[1][0], outcomeReels[1][1], outcomeReels[1][2], winElements);
-    checkBetlineWin(app, config, outcomeReels[2][0], outcomeReels[2][1], outcomeReels[2][2], winElements);
-    checkBetlineWin(app, config, outcomeReels[0][0], outcomeReels[1][1], outcomeReels[2][2], winElements);
-    checkBetlineWin(app, config, outcomeReels[0][2], outcomeReels[1][1], outcomeReels[2][0], winElements);
+    checkBetlineWin(app, config, [outcomeReels[0][0], outcomeReels[0][1], outcomeReels[0][2]], winElements);
+    checkBetlineWin(app, config, [outcomeReels[1][0], outcomeReels[1][1], outcomeReels[1][2]], winElements);
+    checkBetlineWin(app, config, [outcomeReels[2][0], outcomeReels[2][1], outcomeReels[2][2]], winElements);
+    checkBetlineWin(app, config, [outcomeReels[0][0], outcomeReels[1][1], outcomeReels[2][2]], winElements);
+    checkBetlineWin(app, config, [outcomeReels[0][2], outcomeReels[1][1], outcomeReels[2][0]], winElements);
 }
 
 function comparisonFunction(a, b) {
     return a.x - b.x;
 }
 
-function checkBetlineWin(app, config, sprite1, sprite2, sprite3, winElements) {
-    const isWin = sprite1.texture.textureCacheIds[1] === sprite2.texture.textureCacheIds[1]
-        && sprite2.texture.textureCacheIds[1] === sprite3.texture.textureCacheIds[1];
+function checkBetlineWin(app, config, sprites, winElements) {
+    const isWin = sprites.every(sprite => sprite.texture.textureCacheIds[1] === sprites[0].texture.textureCacheIds[1])
 
     if (isWin) {
-        const payout = getPayout(config, sprite1);
+        const payout = getPayout(config, sprites[0]);
         spinWin.value += payout;
         balance.value += payout;
 
-        sprite1.tint = DIM_TINT;
-        sprite2.tint = DIM_TINT;
-        sprite3.tint = DIM_TINT;
+        sprites.forEach(sprite => sprite.tint = DIM_TINT)
+
         winElements.spinWinText.text = WIN_TEXT + spinWin.value;
         winElements.balanceText.text = BALANCE_TEXT + balance.value;
 
-        drawBetline(app, sprite1, sprite2, sprite3);
+        drawBetline(app, sprites);
     }
 }
 
-function drawBetline(app, sprite1, sprite2, sprite3) {
+function drawBetline(app, sprites) {
     const graphics = new Graphics();
 
     graphics.beginFill();
     graphics.lineStyle(4, 0xffffff);
-    graphics.moveTo(LEFT_TOP_CORNER.x + (sprite1.reelX * SYMBOL_SIZE), LEFT_TOP_CORNER.y + (sprite1.reelY * SYMBOL_SIZE));
-    graphics.lineTo(LEFT_TOP_CORNER.x + (sprite2.reelX * SYMBOL_SIZE), LEFT_TOP_CORNER.y + (sprite2.reelY * SYMBOL_SIZE));
-    graphics.lineTo(LEFT_TOP_CORNER.x + (sprite3.reelX * SYMBOL_SIZE), LEFT_TOP_CORNER.y + (sprite3.reelY * SYMBOL_SIZE));
+    graphics.moveTo(LEFT_TOP_CORNER.x + (sprites[0].reelX * SYMBOL_SIZE), LEFT_TOP_CORNER.y + (sprites[0].reelY * SYMBOL_SIZE));
+
+    sprites.forEach((sprite, index) => {
+        if(index > 0) {
+            graphics.lineTo(LEFT_TOP_CORNER.x + (sprite.reelX * SYMBOL_SIZE), LEFT_TOP_CORNER.y + (sprite.reelY * SYMBOL_SIZE));
+        }
+    })
     graphics.endFill();
 
     app.stage.addChild(graphics);
